a43368fb7d71dd96dade896cd03a27a1c36e7d8f
glowxq <glowxq@qq.com>
2025-09-28 15:16:15 +0800

    初始化项目

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.17.2"
+}
+
+group = "com.glowxq.plugs"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+intellij {
+    version.set("2023.2.5")
+    type.set("IC") // Target IDE Platform
+    plugins.set(listOf("java"))
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("232")
+        untilBuild.set("242.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+}
